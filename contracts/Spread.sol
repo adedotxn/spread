9aa4006fb057b50c36e48c59a2e3cd794c48c355
Phyf3 <phade075@gmail.com>
2022-06-01 01:01:08 +0100

    feat : name, symbol, half of batch trasnfer

--- contracts/Spread.sol
+++ contracts/Spread.sol
@@ -4,21 +4,31 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract NestcoinToken is ERC20 {
-  address public _owner;
+contract Spread {
+    ERC20 private _token;
+    uint256 public userCount;
 
-  string public coinName;
-  string public coinSymbol;
-
-
-  //minting the NXT token and assigning it to an owner(i.e the deployer)
-  //the initial batch operator (i.e address that should run the batch transactions is set to the owner(i.e the deployer))
-  constructor() ERC20(coinName, coinSymbol) {
-        _mint(msg.sender, 30000000 * 10 ** 18);
-        _owner = msg.sender;
+    constructor(ERC20 token)  {
+        _token = token;
     }
 
-    //Function to run the batch transactions, return the success and sum of amounts [sent]
+    function getbalance() public view returns(uint) {
+        return _token.balanceOf(msg.sender);
+    }
+
+    function getTokenName() public view returns(string memory) {
+        return _token.name();
+    }
+
+    function getTokenSymbol() public view returns(string memory) {
+        return _token.symbol();
+    }
+
+    function setUserCount(uint _number) public {
+        require(_number >= 2, "too short");
+        userCount = _number;
+    }
+
     function batchTransfer(address[] calldata addressesTo, uint256[] calldata amounts) external 
     returns (uint, bool)
     {
@@ -27,24 +37,15 @@ contract NestcoinToken is ERC20 {
         uint256 sum = 0;
         for(uint256 i = 0; i < addressesTo.length; i++) {
             require(addressesTo[i] != address(0), "Invalid Address");
-            require(amounts[i] != 0, "You cant't trasnfer 0 tokens");
-            require(addressesTo.length <= 200, "exceeds number of allowed addressess");
-            require(amounts.length <= 200, "exceeds number of allowed amounts");
+            require(amounts[i] != 0, "Invalid transfer amount");
+            require(userCount >= 2, "Too short" );
+            require(addressesTo.length <= userCount, "exceeds number of allowed addressess");
+            require(amounts.length <= userCount, "exceeds number of allowed amounts");
             
-            require(transfer(addressesTo[i], amounts[i]* 10 ** 18), "Unable to transfer token to the account");
+            require(_token.transfer(addressesTo[i], amounts[i]* 10 ** 18), "Unable to transfer token to the account");
             sum += amounts[i];
         }
         return(sum, true);
-    }
-
-    //Function to check the remainig token after distribution
-    function checkTokenBalance() public view returns(uint256)  {
-        return balanceOf(msg.sender);
-    }
-
-    //Function to for the current user to check their balance
-    function userBalance() public view returns(uint256) {
-        return balanceOf(msg.sender);
     }
 
 }
